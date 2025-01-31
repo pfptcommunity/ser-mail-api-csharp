7d3c4b6f6385781808aa6427d5ffaa1dc821de86
Ludvik Jerabek <ljerabek@proofpoint.com>
2025-01-31 10:17:02 -0500

    Update Program.cs

--- example/ser-api-test/Program.cs
+++ example/ser-api-test/Program.cs
@@ -44,22 +44,22 @@ message.AddReplyTo(new MailUser("reply_to1@proofpoint.com", "Reply To 1"));
 message.AddReplyTo(new MailUser("reply_to2@proofpoint.com", "Reply To 2"));
 
 // Add Base64 empty attachment, this currently doesn't work with the REST API.
-message.AddAttachment(new Attachment("", "empty.txt", "text/plain", Disposition.Attachment));
+message.AddAttachment(Attachment.FromBase64String("", "empty.txt", "text/plain2", Disposition.Attachment));
 
 // Add Base64 encoded attachment
-message.AddAttachment(new Attachment("VGhpcyBpcyBhIHRlc3Qh", "test.txt", "text/plain", Disposition.Attachment));
+message.AddAttachment(Attachment.FromBase64String("VGhpcyBpcyBhIHRlc3Qh", "test.txt", "text/plain", Disposition.Attachment));
 
 // Add file attachment from disk, if disposition is not passed, the default is Disposition.Attachment
-message.AddAttachment(new FileAttachment(@"C:\temp\file.csv", Disposition.Attachment));
+message.AddAttachment(Attachment.FromFile(@"C:\temp\file.csv", Disposition.Attachment));
 
 // Convert the string to a byte array using UTF8 encoding
 byte[] bytes = Encoding.UTF8.GetBytes("This is a sample text stream.");
 
 // Add byte array as attachment, if disposition is not passed, the default is Disposition.Attachment
-message.AddAttachment(new BinaryAttachment(bytes, "bytes.txt", "text/plain", Disposition.Attachment));
+message.AddAttachment(Attachment.FromBytes(bytes, "bytes.txt", "text/plain", Disposition.Attachment));
 
 // Add empty attachment, this currently doesn't work with the REST API. 
-message.AddAttachment(new BinaryAttachment(Array.Empty<byte>(), "nobytes.txt", "text/plain", Disposition.Attachment));
+message.AddAttachment(Attachment.FromBytes(Array.Empty<byte>(), "nobytes.txt", "text/plain", Disposition.Attachment));
 
 // Print Message object as JSON text
 Console.WriteLine(message.ToString());
