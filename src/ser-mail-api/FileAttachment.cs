aa5e119461b1d3824e35f414ffa4f2b33ba405e9
Ludvik Jerabek <ljerabek@proofpoint.com>
2025-01-31 00:06:16 -0500

    Update FileAttachment.cs

--- src/ser-mail-api/FileAttachment.cs
+++ src/ser-mail-api/FileAttachment.cs
@@ -1,10 +1,11 @@
 ﻿namespace Proofpoint.SecureEmailRelay.Mail
 {
-    public class FileAttachment : Attachment
+    public sealed class FileAttachment : Attachment
     {
         public FileAttachment(string filePath, Disposition disposition = Disposition.Attachment, bool allowFallbackMimeType = false)
             : this(filePath, GetMimeType(filePath, allowFallbackMimeType), disposition)
-        { }
+        {
+        }
 
         public FileAttachment(string filePath, string mimeType, Disposition disposition = Disposition.Attachment, bool validateMimeType = true)
             : base(EncodeFileContent(filePath), Path.GetFileName(filePath), mimeType, disposition)
