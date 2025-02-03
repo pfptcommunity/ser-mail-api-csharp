cd2372b73fe8b0879a48147095b44b1bb43927e5
Ludvik Jerabek <ljerabek@proofpoint.com>
2025-02-03 00:39:54 -0500

    Update Program.cs

--- example/ser-api-test/Program.cs
+++ example/ser-api-test/Program.cs
@@ -20,6 +20,7 @@ else
     Environment.Exit(1);
 }
 
+
 // Create a new Message object
 Message message = new("This is a test email", new MailUser("sender@proofpoint.com", "Joe Sender"));
 
@@ -44,22 +45,22 @@ message.AddReplyTo(new MailUser("reply_to1@proofpoint.com", "Reply To 1"));
 message.AddReplyTo(new MailUser("reply_to2@proofpoint.com", "Reply To 2"));
 
 // Add Base64 empty attachment, this currently doesn't work with the REST API.
-message.AddAttachment(Attachment.FromBase64String("", "empty.txt", "text/plain", Disposition.Attachment));
+// message.AddAttachment(Attachment.CreateBuilder().FromBase64("").SetFilename("empty.txt").SetMimeType("text/plain").Build());
 
 // Add Base64 encoded attachment
-message.AddAttachment(Attachment.FromBase64String("VGhpcyBpcyBhIHRlc3Qh", "test.txt", "text/plain", Disposition.Attachment));
+message.AddAttachment(Attachment.CreateBuilder().FromBase64("VGhpcyBpcyBhIHRlc3Qh").SetFilename("empty.txt").SetMimeType("text/plain").Build());
 
 // Add file attachment from disk, if disposition is not passed, the default is Disposition.Attachment
-message.AddAttachment(Attachment.FromFile(@"C:\temp\file.csv", Disposition.Attachment));
+message.AddAttachment(Attachment.CreateBuilder().FromFile(@"C:\temp\file.csv").SetDisposition(Disposition.Inline).Build());
 
 // Convert the string to a byte array using UTF8 encoding
 byte[] bytes = Encoding.UTF8.GetBytes("This is a sample text stream.");
 
 // Add byte array as attachment, if disposition is not passed, the default is Disposition.Attachment
-message.AddAttachment(Attachment.FromBytes(bytes, "bytes.txt", "text/plain", Disposition.Attachment));
+message.AddAttachment(Attachment.CreateBuilder().FromBytes(bytes).SetFilename("bytes.txt").SetMimeType("text/plain").Build());
 
 // Add empty attachment, this currently doesn't work with the REST API. 
-message.AddAttachment(Attachment.FromBytes(Array.Empty<byte>(), "nobytes.txt", "text/plain", Disposition.Attachment));
+// message.AddAttachment(Attachment.CreateBuilder().FromBytes(Array.Empty<byte>()).SetFilename("nobytes.txt").SetMimeType("text/plain").Build());
 
 // Print Message object as JSON text
 Console.WriteLine(message.ToString());
