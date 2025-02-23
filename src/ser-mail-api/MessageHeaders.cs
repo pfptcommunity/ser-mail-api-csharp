d41d747ca7b60dcfad518d6d5efeaf5deaca735f
Ludvik Jerabek <ljerabek@proofpoint.com>
2025-02-23 15:40:59 -0500

    Add comments

--- src/ser-mail-api/MessageHeaders.cs
+++ src/ser-mail-api/MessageHeaders.cs
@@ -2,10 +2,20 @@
 
 namespace Proofpoint.SecureEmailRelay.Mail
 {
+    /// <summary>
+    /// Represents the headers of a mail message, specifically the sender information.
+    /// </summary>
     public sealed class MessageHeaders
     {
+        /// <summary>
+        /// The backing field for the <see cref="From"/> property.
+        /// </summary>
         private MailUser _from = null!;
 
+        /// <summary>
+        /// Gets or sets the sender of the message as specified in the headers.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">Thrown when the value is set to null.</exception>
         [JsonPropertyName("from")]
         public MailUser From
         {
@@ -13,6 +23,11 @@ namespace Proofpoint.SecureEmailRelay.Mail
             set => _from = value ?? throw new ArgumentNullException(nameof(value), "Header from address must not be null.");
         }
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="MessageHeaders"/> with the specified sender.
+        /// </summary>
+        /// <param name="from">The sender of the message as specified in the headers.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="from"/> is null.</exception>
         public MessageHeaders(MailUser from)
         {
             From = from;
